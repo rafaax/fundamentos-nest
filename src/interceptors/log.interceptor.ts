acb5d509dfa437110008d84bbd392de129719e64
raphael <rafavfx1@gmail.com>
2024-08-10 14:25:51 -0300

    adicionando log de request

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -5,9 +5,18 @@ import { Observable } from "rxjs";
 export class LogInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler <any>): Observable <any> | Promise <Observable<any>> {
 
-        const date = Date.now();
+        const date = Date.now(); // tempo que interceptor foi observado
+
+
         return next.handle().pipe(tap(() => {
+
+            const request = context.switchToHttp().getRequest();
+            
+            console.log( request.method + ' ' +  request.url);
+            console.log(request.body)
+
             console.log(`Execução levou ${Date.now() - date} milisegundos..`)
+
         }))
     }
 }
