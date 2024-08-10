979487a9f700a3c0805717a0f2684d33f95d65b2
raphael <rafavfx1@gmail.com>
2024-08-10 14:47:38 -0300

    adicionado o middleware no modulo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,9 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import { PrismaModel } from "src/prisma/prisma.module";
+import { UserIdCheckMiddleware } from "src/middlewares/user-id-check.middleware";
 
 @Module({
     imports: [PrismaModel], 
@@ -10,4 +11,11 @@ import { PrismaModel } from "src/prisma/prisma.module";
     providers: [UserService], 
     exports: [],
 }) 
-export class UserModule {}
+export class UserModule implements NestModule {
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(UserIdCheckMiddleware).forRoutes(
+            {path: 'users/:id', method: RequestMethod.ALL}, 
+        )
+    }
+}
